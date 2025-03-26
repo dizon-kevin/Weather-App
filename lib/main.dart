9c797c25c96d1706fa5e806e42737bf867132e84
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-03-26 19:38:15 +0800

    edited by mac and added settings.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'dart:convert';
-
+import 'settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 
-
-
-void main()=> runApp(
-    CupertinoApp(
-      debugShowCheckedModeBanner: false,
-      home: MyApp(),));
+void main() => runApp(
+  CupertinoApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ),
+);
 
 class MyApp extends StatefulWidget {
-
   const MyApp({super.key});
 
   @override
@@ -19,83 +18,57 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Map<String, dynamic> weatherData = {};
-
-  String city = "Loading....";
-  String temp = "";
+  String city = "Arayat"; // Default city
+  double temp = 0.0;
   String humidity = "---";
   String feelsLike = "---";
   IconData? weatherIcon;
-  String url = "https://api.openweathermap.org/data/2.5/weather?q=Arayat&appid=b2aa11fa10d2583b6a1651a3d1f6d391";
-
-  Future<void> getData() async {
-    final response = await http.get(
-      Uri.parse(url),
-    );
-
-    setState(() {
-      weatherData = jsonDecode(response.body);
-      city = weatherData["name"];
-      temp = (weatherData["main"]["temp"] - 273.15).toStringAsFixed(1) + "°";
-      humidity = weatherData["main"]["humidity"].toString();
-      feelsLike = (weatherData["main"]["feels_like"] - 273.15).toStringAsFixed(1) + "%";
-
-      if (weatherData["weather"][0]["main"] == "Clear") {
-        weatherIcon = CupertinoIcons.sun_max;
-      } else if (weatherData["weather"][0]["main"] == "Clouds") {
-        weatherIcon = CupertinoIcons.cloud;
-      }
-    });
-    print(weatherData["weather"][0]["main"]);
-
-  }
-  @override
+  bool isMetric = true; // Default to Celsius
 
   @override
   void initState() {
-    getData();
     super.initState();
+    getData(city);
   }
-  Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        navigationBar: CupertinoNavigationBar(
-          backgroundColor: CupertinoColors.black,
-          middle: Text(
-            "iWeather",
-            style: TextStyle(color: CupertinoColors.white, fontSize: 18),
-          ),
-          trailing: CupertinoButton(
-            padding: EdgeInsets.zero,
-            child: Icon(
-              CupertinoIcons.settings,
-              size: 19,
-              color: CupertinoColors.systemPurple,
-            ),
-            onPressed: () {
-              // Add your navigation to the settings page here.
-            },
-          ),
-        ),
 
-        child: SafeArea(child: Center(
-      child: Column(
-        children: [
-            SizedBox(height: 100,),
-          Text('$city', style: TextStyle(fontSize: 50, fontWeight: FontWeight.w100),),
-          Text(' $temp', style: TextStyle(fontSize: 20),),
-          Icon(weatherIcon, color: CupertinoColors.systemPurple, size: 90,),
-          SizedBox(height: 50,),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("Humidity: $humidity"),
-              SizedBox(width: 100,),
-              Text('Feels Like: $feelsLike')
-            ],
-          )
+  Future<void> getData(String newCity) async {
+    final apiKey = "b2aa11fa10d2583b6a1651a3d1f6d391";
+    final url =
+        "https://api.openweathermap.org/data/2.5/weather?q=$newCity&appid=$apiKey";
 
-        ],
-      ),
-    )));
+    try {
+      final response = await http.get(Uri.parse(url));
+
+      if (response.statusCode == 200) {
+        final weatherData = jsonDecode(response.body);
+        setState(() {
+          city = weatherData["name"];
+          double kelvinTemp = weatherData["main"]["temp"];
+          double kelvinFeelsLike = weatherData["main"]["feels_like"];
+          temp = isMetric
+              ? kelvinTemp - 273.15 // Celsius
+              : (kelvinTemp - 273.15) * 9 / 5 + 32; // Fahrenheit
+
+          humidity = weatherData["main"]["humidity"].toString();
+          feelsLike = isMetric
+              ? (kelvinFeelsLike - 273.15).toStringAsFixed(1) + "°C"
+              : ((kelvinFeelsLike - 273.15) * 9 / 5 + 32).toStringAsFixed(1) +
+              "°F";
+
+          // Set weather icon
+          String weatherCondition = weatherData["weather"][0]["main"];
+          if (weatherCondition == "Clear") {
+            weatherIcon = CupertinoIcons.sun_max;
+          } else if (weatherCondition == "Clouds") {
+            weatherIcon = CupertinoIcons.cloud;
+          } else {
+            weatherIcon = CupertinoIcons.cloud_rain;
+          }
+        });
+      } else {
+        showErrorDialog(newCity);
+      }
+    } catch (e) {
+      showErrorDialog(newCity);
+    }
   }
-}
