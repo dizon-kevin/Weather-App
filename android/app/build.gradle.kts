8639f78239878b62905aee52decf9859af9f5944
macapagal-marc-lawrence <macapagalmarck@gmail.com>
2025-03-27 00:30:07 +0800

    codemagic configuration by marc

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.iweather.app"
+    namespace = "com.iweather.devops"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -20,7 +20,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.iweather.app"
+        applicationId = "com.iweather.devops"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
