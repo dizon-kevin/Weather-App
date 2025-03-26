ca61750ec436106e00284acce532bc459b12d203
cruz-john-eric <johnericcruz1604@gmail.com>
2025-03-26 19:52:29 +0800

    commited by eric

--- lib/settings.dart
+++ lib/settings.dart
@@ -33,4 +33,53 @@ class _SettingsPageState extends State<SettingsPage> {
       isMetric = newMetric;
     });
   }
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.black,
+        middle: Text("Settings", style: TextStyle(color: CupertinoColors.white)),
+      ),
+      child: SafeArea(
+        child: Column(
+          children: [
+            CupertinoListTile(
+              title: Text("Location"),
+              leading: Icon(CupertinoIcons.location),
+              trailing: CupertinoButton(
+                child: Text(city),
+                onPressed: () {
+                  showCupertinoDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      TextEditingController controller = TextEditingController(text: city);
+                      return CupertinoAlertDialog(
+                        title: Text("Enter Location"),
+                        content: CupertinoTextField(
+                          controller: controller,
+                          onChanged: (value) {
+                            city = value;
+                          },
+                        ),
+                        actions: [
+                          CupertinoDialogAction(
+                            child: Text("Cancel"),
+                            isDestructiveAction: true,
+                            onPressed: () => Navigator.pop(context),
+                          ),
+                          CupertinoDialogAction(
+                            child: Text("OK"),
+                            onPressed: () {
+                              updateSettings(city, isMetric);
+                              Navigator.pop(context);
+                            },
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+
 
